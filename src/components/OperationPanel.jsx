e335fdf99c3a99c585b82ee0ff19f1528742d298
Tomasz Michalak <vlad3dracula@gmail.com>
2025-04-24 18:41:04 +0200

    addd tooltips

--- src/components/OperationPanel.jsx
+++ src/components/OperationPanel.jsx
@@ -1,40 +1,51 @@
-import { Card, Button, Switch, Typography, Box, Stack } from "@mui/material";
-import FlashOffIcon from "@mui/icons-material/FlashOff";
-import { useEffect } from "react";
-import container from "../container/container";
-import useDeviceStore from "../store/deviceStore.js";
+import {
+  Card,
+  Button,
+  Switch,
+  Typography,
+  Box,
+  Stack,
+  Tooltip,
+} from "@mui/material"
+import FlashOffIcon from "@mui/icons-material/FlashOff"
+import { useEffect } from "react"
+import container from "../container/container"
+import useDeviceStore from "../store/deviceStore.js"
 
 const OperationPanel = () => {
-  const dataManager = container.resolve('dataManager')
-  const isSchedulerEnabled = useDeviceStore(state => state.isSchedulerEnabled)
-  const useWeatherAssistant = useDeviceStore(state => state.useWeatherAssistant)
-  
+  const dataManager = container.resolve("dataManager")
+  const isSchedulerEnabled = useDeviceStore(
+    (state) => state.isSchedulerEnabled
+  )
+  const useWeatherAssistant = useDeviceStore(
+    (state) => state.useWeatherAssistant
+  )
+
   useEffect(() => {
-    const fetchStates = async() => {
+    const fetchStates = async () => {
       await dataManager.refreshIsSchedulerEnabled()
       await dataManager.refreshUseWeatherAssistant()
     }
-    
+
     fetchStates()
-    
   }, [])
 
   const onCloseAllClick = () => {
-     dataManager.closeAll()
+    dataManager.closeAll()
   }
 
   const onSchedulerEnableChange = async () => {
-    if (isSchedulerEnabled){
+    if (isSchedulerEnabled) {
       await dataManager.stopScheduler()
     } else {
       await dataManager.runScheduler()
     }
-    
+
     await dataManager.refreshIsSchedulerEnabled()
-  };
+  }
 
   const onUseWeatherAssistantChange = async () => {
-    if (useWeatherAssistant){
+    if (useWeatherAssistant) {
       await dataManager.setUseWeatherAssistant(false)
     } else {
       await dataManager.setUseWeatherAssistant(true)
@@ -43,28 +54,42 @@ const OperationPanel = () => {
   }
 
   return (
-    <Card sx={{ p: 2 }} >
-      <Stack direction="row" spacing={2} >
+    <Card sx={{ p: 2 }}>
+      <Stack direction="row" spacing={2}>
         <Box>
-          <Switch
-            checked={isSchedulerEnabled}
-            onChange={onSchedulerEnableChange}
-          />
+          <Tooltip title="Enable/Disable task scheduler." placement="top">
+            <Switch
+              checked={isSchedulerEnabled}
+              onChange={onSchedulerEnableChange}
+            />
+          </Tooltip>
           <Typography>Scheduler</Typography>
         </Box>
         <Box>
-          <Switch
-            checked={useWeatherAssistant}
-            onChange={onUseWeatherAssistantChange}
-          />
+          <Tooltip
+            title="Enable/Disable weather service which predict whether it should water or not."
+            placement="top"
+          >
+            <Switch
+              checked={useWeatherAssistant}
+              onChange={onUseWeatherAssistantChange}
+            />
+          </Tooltip>
           <Typography>Weather assistant</Typography>
         </Box>
-        <Button onClick={onCloseAllClick} variant="outlined" startIcon={<FlashOffIcon />} sx={{ m: 2 }}>
-          Close all
-        </Button>
+        <Tooltip title="Close all valves" placement="top">
+          <Button
+            onClick={onCloseAllClick}
+            variant="outlined"
+            startIcon={<FlashOffIcon />}
+            sx={{ m: 2 }}
+          >
+            Close all
+          </Button>
+        </Tooltip>
       </Stack>
     </Card>
-  );
-};
+  )
+}
 
-export default OperationPanel;
+export default OperationPanel
